68941261354c2af2c1f1276612381b69c41eb22c
syedfaraz0099-sketch <syedfaraz0099@gmail.com>
2026-03-08 12:37:48 +0530

    Update page.tsx

--- frontend/app/dashboard/caption/page.tsx
+++ frontend/app/dashboard/caption/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from "react"
-
+import { supabase } from "../../lib/supabase"
 export default function CaptionPage() {
   const [topic, setTopic] = useState("")
   const [caption, setCaption] = useState("")
